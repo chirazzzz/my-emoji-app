ad610892a62561cd6914b7d3f8aabca75b8a0e06
Chirag Mehta <chiragmehta.uk@gmail.com>
2021-01-21 18:05:43 +0000

    Initialised Project files
    Downloaded files from a Scrimba cast and initialised this repo with them.

---
+++ index.js
@@ -0,0 +1,46 @@
+// Make the pop and shift buttons work as well
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiInput = document.getElementById("emoji-input")
+
+function renderEmojis() {
+    const emojiContainer = document.getElementById("emoji-container")
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+
+renderEmojis()
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+const shiftBtn = document.getElementById("shift-btn")
+shiftBtn.addEventListener("click", function(){
+    myEmojis.shift
+    renderEmojis()
+})
+
+const popBtn = document.getElementById("pop-btn")
+popBtn.addEventListener("click", function(){
+    myEmojis.pop
+    renderEmojis()
+})
